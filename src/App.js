b1b9e68bc33f2a86facae67c2c6178ec326c3bed
Osajulian <you@example.com>
2018-10-09 08:38:56 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import Counter from './counter';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+//Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    countValue: state.count
+  };
 }
 
-export default App;
+//Action
+let increaseAction = { type: "increase" };
+let decreaseAction = { type: "decrease" };
+
+//map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function () {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function () {
+      return dispatch(decreaseAction);
+    }
+  };
+}
+
+//The HOC
+let connectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+
+export default connectedComponent;
+
